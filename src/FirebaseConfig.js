da68bd56374bb7f086540f7fa151504cc45e67ec
domenkola <domenkola@gmail.com>
2025-11-05 22:42:57 +0000

    dodan firebase API

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBHJjzIol6VOpyFFJMhdGEbDFjd16IVtZo",
+  authDomain: "faksapp-a6407.firebaseapp.com",
+  projectId: "faksapp-a6407",
+  storageBucket: "faksapp-a6407.firebasestorage.app",
+  messagingSenderId: "411157253911",
+  appId: "1:411157253911:web:75e9d6271a684817503152",
 };
 
 const app = initializeApp(firebaseConfig);
