1626452ae90c248e5a06119cb88df7ce9d5fdd29
domenkola <domenkola@gmail.com>
2025-11-05 22:34:21 +0000

    dodajanje parametrov

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,9 @@
 const functions = require("firebase-functions");
 const fetch = require("node-fetch");
 
-const client_id = "YOUR_SPOTIFY_CLIENT_ID";
+const client_id = "8eafb21898ad4584a990e71f2e2327cc";
 const client_secret = "YOUR_SPOTIFY_CLIENT_SECRET";
-const redirect_uri = "YOUR_FIREBASE_HOSTING_URL"; // npr. https://myapp.web.app/
+const redirect_uri = "https://faksapp-35376630.web.app/"; // npr. https://myapp.web.app/
 
 exports.spotifyAuth = functions.https.onRequest(async (req, res) => {
   const code = req.query.code;
